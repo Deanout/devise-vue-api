e71d20cb7952341912e9dba70f48275458086c6e
Dean DeHart <DeHartDean@gmail.com>
2022-01-17 05:59:22 -0500

    Final commit pre publish

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -12,10 +12,10 @@ class MembersController < ApplicationController
 
   private
 
-  def get_user_from_token
+  def login_user_from_api_token
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
                              Rails.application.credentials.devise[:jwt_secret_key]).first
     user_id = jwt_payload['sub']
-    user = User.find(user_id.to_s)
+    User.find(user_id.to_s)
   end
 end
